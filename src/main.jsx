8b94500338835e24b0cac41781208eef2b593894
amirmahdighadiri <amirmahdiighadirii@gmail.com>
2025-11-12 23:42:55 +0330

    \auto update"

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,6 @@ import './index.css'
 import App from './App.jsx'
 import {HashRouter} from "react-router-dom";
 
-if (!window.location.hash) {
-    window.location.replace(window.location.href + '#/');
-}
-
 createRoot(document.getElementById('root')).render(
     <HashRouter>
         <App />
